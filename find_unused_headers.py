bb95aa93560d048728f974ad9a262f7dd5a8a25c
Johannes Spohr <jones@blackpantsstudio.com>
2017-03-10 12:38:51 +0100

    Added find_unused_headers.py

---
+++ find_unused_headers.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os, re, sys
+from optparse import OptionParser
+
+parser = OptionParser()
+
+
+def findFiles(path, regex):
+  res = []
+  for root, dirs, fnames in os.walk(path):
+    for fname in fnames:
+      if regex.match(fname):
+        res.append(os.path.join(root, fname))
+  return res
+
+
+if __name__ == "__main__":
+  (options, args) = parser.parse_args()
+
+  sourceFileRegEx = re.compile(".*\.(c|cpp|h|hpp|m|mm)")
+  headerFileRegEx = re.compile(".*\.(h|hpp)")
+  paths = args
+  files = []
+  for p in paths:
+    files += findFiles(p, sourceFileRegEx)
+
+  includeRegEx = re.compile('^#[ \t]*include[ \t]*[<"](.*)[>"]')
+  includes = set()
+  for file in files:
+    f = open(file)
+    for line in f:
+      match = includeRegEx.match(line)
+      if match:
+        filename = os.path.basename(match.group(1))
+        includes.add(filename)
+
+  includedFiles = []
+  for file in files:
+    filename = os.path.basename(file)
+    if filename in includes:
+      includedFiles.append(file)
+
+  for file in files:
+    if headerFileRegEx.match(file) and file not in includedFiles:
+      print(file)
+
+  sys.exit(0)
